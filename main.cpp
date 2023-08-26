0f721fb0ec1dc7d2a24fc49772a99503f88f1059
unknown <fran.mauri2000@gmail.com>
2023-08-26 19:50:00 -0300

    Abrir un archivo con argumentos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include "src/Lexer.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+	if (argc < 2)
+	{
+		cout << "Ingrese un nombre de archivo" << endl;
+		return 1;
+	}
+
+
+    fstream input;
+    input.open(argv[1], ios::in);
+
+    string temp, src = "";
+    
+    if(input.good()) {
+        while(!input.eof()) {
+            getline(input, temp);
+            src += temp;
+        }
+        input.close();
+    }
+    else {
+        cout << "Error al abrir el archivo" << endl;
+    }
+
+    src += '\0';
+
+    cout << src << endl;
+
+    Lexer *lexer = new Lexer(src);
+    lexer->run();
+    
 }
