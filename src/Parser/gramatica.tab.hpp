034aa41da3af9afab44ce13202ecbe99258c0c0e
unknown <fran.mauri2000@gmail.com>
2023-09-30 20:45:52 -0300

    Correcciones en la gramatica

---
+++ src/Parser/gramatica.tab.hpp
@@ -0,0 +1,94 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     SHORT = 258,
+     UINT = 259,
+     DOUBLE = 260,
+     VOID = 261,
+     STRING = 262,
+     CLASS = 263,
+     INTERFACE = 264,
+     IMPLEMENT = 265,
+     IMPL = 266,
+     IF = 267,
+     ELSE = 268,
+     END_IF = 269,
+     PRINT = 270,
+     FOR = 271,
+     IN = 272,
+     RANGE = 273,
+     RETURN = 274,
+     LESS_EQUAL = 275,
+     GREATER_EQUAL = 276,
+     EQUAL = 277,
+     NOT_EQUAL = 278,
+     PLUS_EQUAL = 279,
+     UMINUS = 280,
+     ID = 281,
+     CTE = 282
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 17 ".\\gramatica.ypp"
+
+        int number = 0;
+        char *string;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 86 "gramatica.tab.hpp"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
