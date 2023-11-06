df7b0791112584f8c45cd66beb59115a12cca7b5
unknown <fran.mauri2000@gmail.com>
2023-11-06 02:38:55 -0300

    Eliminar operador auxiliar de tercetos

--- src/IntermediateCodeGenerator/Terceto.h
+++ src/IntermediateCodeGenerator/Terceto.h
@@ -12,19 +12,16 @@ class Terceto{
         string getOp() ;
         string getOp1() ;
         string getOp2();
-        string getOp3();
 
         void setOp(string op);
         void setOp1(string op1);
         void setOp2(string op2);
-        void setOp3(string op3);
 
         static void completarTerceto(int numeroTerceto); // Completar terceto incompleto, backpatching
     private:
         string op;
         string op1; // Operador 1, puede ser otro terceto
         string op2;
-        string op3; // Operador auxiliar
 };
 
 #endif
